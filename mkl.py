c3031184f78210623bbb53b860e5b9c217891046
J0sephPeter <josephpeterhalaka@gmail.com>
2025-11-17 14:17:39 +0200

    Initial commit

---
+++ mkl.py
@@ -0,0 +1,22 @@
+# Go to" the next room and shut the door, while doing so don't forget to get the keys, if got your parent's keys, don't forget to close the windows as well'
+var = "Go to the next room and shut the door, while doing so don't forget to get the keys, if got your parent's keys, don't forget to close the windows as well"
+words = var.split()
+tupled = tuple(words)
+print(tupled)
+    
+print(var)
+
+for letter in var :
+    if letter=="o":
+        print("hello")
+
+
+
+# niu = print(var.count("o"))
+# print(niu)
+# print("hello"*niu)
+
+# hr = "hello world my name is e"
+# r = print(hr.count("e"))
+# i = print(r)
+# print("hello wolrd " * int(i))
